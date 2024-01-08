35c8addf87a3997d3b4edb1a04c31026e2544731
muhamadsuharto <145425126+muhamadsuharto@users.noreply.github.com>
2024-01-08 15:43:38 +0700

    Add files via upload

---
+++ menuawal.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Tugas UTS</title>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="icon" href="image/logo-removebg-preview.png" />
+
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,700;1,100;1,400;1,500;1,600;1,700&display=swap"
+      rel="stylesheet" />
+
+    <!-- icon -->
+    <script src="https://unpkg.com/feather-icons"></script>
+  </head>
+  <body>
+    <!-- navigation bar -->
+    <nav class="navbar">
+      <a href="#" class="navbar-logo">MHT<span>Sensor</span></a>
+
+      <div class="navbar-nav">
+        <a href="#home">HOME</a>
+        <a href="#about">ABOUT</a>
+        <a href="#produk">PRODUCT</a>
+        <a href="#info">LOCATION</a>
+        <a href="index.php">EXIT</a>
+      </div>
+
+      <div class="navbar-ekstra">
+        <a href="#hamburger-menu" id="hamburger-menu"
+          ><i data-feather="menu"></i
+        ></a>
+      </div>
+    </nav>
+
+    <!-- halaman utama -->
+    <section class="hero" id="home">
+      <main class="content">
+        <h1>Mari Beli Di <span>MHTSensor</span></h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi,
+          quibusdam.
+        </p>
+        <a href="#produk" class="tombol">Order</a>
+      </main>
+    </section>
+
+    <!-- Tentang kami start -->
+    <section class="container-about" id="about">
+      <h2><span>About</span> Me</h2>
+      <main class="content-about">
+        <p>
+          Toko Kami Menyediakan Sensor Arduino dengan harga yang murah dengan
+          kualitas bagus sesuai dengan kantong anda dan banyak Promo Ditoko
+          kami<br />
+          Ayo Beli Sekarang!!
+        </p>
+        <br />
+        <h3>Information</h3>
+        <div class="sosmed">
+          <a href="#" class="instagram"
+            ><img
+              src="image/logo instagram hitam yogiancreative.png"
+              alt=""
+              width="40rem"
+              height="40rem"
+          /></a>
+          <a
+            href="#"
+            class="facebook">
+            <img src="image/facebook.png" alt="" width="40rem" height="40rem"
+          /></a>
+          <a href="#" class="wa"
+            ><img src="image/logowa.png" alt="" width="40rem" height="40rem"
+          /></a>
+        </div>
+      </main>
+    </section>
+    <!-- tentang kami end -->
+    <section class="produk" id="produk">
+      <div class="produk-grid">
+        <div class="produk-item">
+          <img src="image/sensordht11.jpeg" alt="produk 1" />
+          <p>Sensor Dht11</p>
+          <p><span>Rp. 14.175</span></p>
+          <a href="order/index.php" class="btn">Order</a>
+        </div>
+        <div class="produk-item">
+          <img src="image/lcd1602+I2C.jpeg" alt="produk 2" />
+          <p>LCD1602 + I2C</p>
+          <p><span>Rp. 30.000</span></p>
+          <a href="order/index.php" class="btn">Order</a>
+        </div>
+        <div class="produk-item">
+          <img src="image/sensor Ultrasonik.jpeg" alt="produk 3" />
+          <p>Ultrasonic Sensor Module HC-SR04</p>
+          <p><span>Rp. 10.000</span></p>
+          <a href="order/index.php" class="btn">Order</a>
+        </div>
+        <div class="produk-item">
+          <img src="image/sensor pir.jpeg" alt="produk 4" />
+          <p>HC-SR501 PIR Motion Sensor Detector Module</p>
+          <p><span>Rp. 10.000</span></p>
+          <a href="order/index.php" class="btn">Order</a>
+        </div>
+        <div class="produk-item">
+          <img src="image/sensor flame detector.jpeg" alt="produk 5" />
+          <p>IR Infrared Flame Detection Sensor</p>
+          <p><span>Rp. 6.000</span></p>
+          <a href="order/index.php" class="btn">Order</a>
+        </div>
+        <div class="produk-item">
+          <img src="image/sensor suara.jpeg" alt="produk 6" />
+          <p>Sound Detection Sensor Module Suara</p>
+          <p><span>Rp. 6.825</span></p>
+          <a href="order/index.php" class="btn">Order</a>
+        </div>
+        <div class="produk-item">
+          <img src="image/sensor obstacle.jpeg" alt="Game 6" />
+          <p</>Obstacle Avoidance Sensor Module Infrared</p>
+          <p><span>Rp. 5.250</span></p>
+          <a href="order/index.php" class="btn">Order</a>
+        </div>
+        <div class="produk-item">
+          <img src="image/sensor gas.jpeg" alt="Game 6" />
+          <p>Sensor gas mudah terbakar asap gas metana MQ-2</p>
+          <p><span>Rp. 14.700</p>
+          <a href="order/index.php" class="btn">Order</a>
+        </div>
+      </div>
+    </section>
+
+    <section class="info" id="info">
+      <div class="container-info">
+        <div class="judul-info">
+          <h2>Information</h2>
+        </div>
+        <div class="isi-info">
+          <ul>
+            <li>Contact : <a href="https://api.whatsapp.com/send?phone=6282314940695">082314940695</a></li>
+            <li>facebook : <a href="#">-</a></li>
+          </ul>
+        </div>
+        <div class="location">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12582.546258349115!2d109.37725920573145!3d-6.89487010759885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6fc5334bf004a5%3A0x953336debf5dffe5!2sPemalang%2C%20Kec.%20Pemalang%2C%20Kabupaten%20Pemalang%2C%20Jawa%20Tengah!5e0!3m2!1sid!2sid!4v1699424763607!5m2!1sid!2sid"
+            width="600"
+            height="450"
+            style="border: 0"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+      </div>
+    </section>
+
+    <!-- footer -->
+    <footer class="footer">
+      <div class="container-footer">
+        <h3>Senang Belanja Disini Dan Terima Kasih Telah Mengunjungi web</h3>
+        <a href="https://api.whatsapp.com/send?phone=6282314940695&text=Saya%20ingin%20bertanya%20tentang%20Sensor.">Hubungi Kami jika ingin bertanya</a>
+        <h5>@Created BySuharto</h5>
+      </div>
+    </footer>
+
+    <!-- icon -->
+    <script>
+      feather.replace();
+    </script>
+
+    <script src="js/script.js"></script>
+  </body>
+</html>
